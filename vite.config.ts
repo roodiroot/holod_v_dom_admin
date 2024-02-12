91746ee52eee8da42b1a35ace24085996f03b3fa
Borisov Maxim <voodivood@gmail.com>
2024-02-12 14:59:30 +0300

    host

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
+    host: "holod-vdom.store",
     proxy: {
       "/api": {
         target: "https://api.holod-vdom.ru",
