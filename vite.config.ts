b5f770660f87855c6bbe24236b827aac39fde7a9
Borisov Maxim <voodivood@gmail.com>
2024-02-12 15:33:16 +0300

    f

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // preview: {
-  //   host: "holod-vdom.store",
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://api.holod-vdom.ru",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+  preview: {
+    port: 3001,
+    proxy: {
+      "/api": {
+        target: "https://api.holod-vdom.ru",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   server: {
     port: 3001,
     proxy: {
